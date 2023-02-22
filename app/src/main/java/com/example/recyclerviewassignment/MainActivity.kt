051590f0826bc941dec7481b204f0248fa75efcf
iaheung <tur20579@temple.edu>
2023-02-22 14:54:03 +0900

    Added images to application

--- app/src/main/java/com/example/recyclerviewassignment/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewassignment/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -23,14 +24,36 @@ class MainActivity : AppCompatActivity() {
         // we can just use an array of integers instead
 
         val images = arrayOf(
-            ImageItem(R.drawable.android_01, "Lake"),
-            ImageItem(R.drawable.android_02, "Daisies"),
-            ImageItem(R.drawable.android_03, "Car lights"),
-            ImageItem(R.drawable.android_04, "RV trailer"),
-            ImageItem(R.drawable.android_05, "Seaside road"),
-            ImageItem(R.drawable.android_06, "Autumn leaves"),
-            ImageItem(R.drawable.android_07, "Colorful houses"),
-            ImageItem(R.drawable.android_08, "Road")
+            ImageItem(R.drawable.atlantahawks, "Atlanta Hawks"),
+            ImageItem(R.drawable.bostonceltics, "Boston Celtics"),
+            ImageItem(R.drawable.brooklynnets, "Brooklyn Nets"),
+            ImageItem(R.drawable.charlottehornets, "Charlotte Hornets"),
+            ImageItem(R.drawable.chicagobulls, "Chicago Bulls"),
+            ImageItem(R.drawable.clevelandcavaliers, "Cleveland Cavaliers"),
+            ImageItem(R.drawable.dallasmavericks, "Dallas Mavericks"),
+            ImageItem(R.drawable.denvernuggets, "Denver Nuggets"),
+            ImageItem(R.drawable.detroitpistons, "Detroit Pistons"),
+            ImageItem(R.drawable.goldenstatewarriors, "Golden State Warriors"),
+            ImageItem(R.drawable.houstonrockets, "Houston Rockets"),
+            ImageItem(R.drawable.indianapacers, "Indiana Pacers"),
+            ImageItem(R.drawable.losangelesclippers, "Los Angeles Clippers"),
+            ImageItem(R.drawable.losangeleslakers, "Los Angeles Lakers"),
+            ImageItem(R.drawable.memphisgrizzlies, "Memphis Grizzlies"),
+            ImageItem(R.drawable.miamiheat, "Miami Heat"),
+            ImageItem(R.drawable.milwaukeebucks, "Milwaukee Bucks"),
+            ImageItem(R.drawable.minnesotatimberwolves, "Minnesota Timberwolves"),
+            ImageItem(R.drawable.neworleanspelicans, "New Orleans Pelicans"),
+            ImageItem(R.drawable.newyorkknicks, "New York Knicks"),
+            ImageItem(R.drawable.oklahomacitythunder, "Oklahoma City Thunder"),
+            ImageItem(R.drawable.orlandomagic, "Orlando Magic"),
+            ImageItem(R.drawable.philadelphia76ers, "Philadelphia 76ers"),
+            ImageItem(R.drawable.phoneixsuns, "Phoenix Suns"),
+            ImageItem(R.drawable.portlandtrailblazers, "Portland Trailblazers"),
+            ImageItem(R.drawable.sacramentokings, "Sacramento Kings"),
+            ImageItem(R.drawable.sanantoniospurs, "San Antonio Spurs"),
+            ImageItem(R.drawable.torontoraptors, "Toronto Raptors"),
+            ImageItem(R.drawable.utahjazz, "Utah Jazz"),
+            ImageItem(R.drawable.washingtonwizards, "Washington Wizards"),
         )
 
         val callback = { item: ImageItem ->
@@ -41,8 +64,7 @@ class MainActivity : AppCompatActivity() {
         recyclerView.adapter = ImageAdapter(images, callback)
 
         // Add a layout manager, which requires a context, this (main)
-
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        //recyclerView.layoutManager = GridLayoutManager(this, 2)
+        // recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = GridLayoutManager(this, 3)
     }
 }
